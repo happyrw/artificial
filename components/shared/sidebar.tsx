8fc51f6b4bc41b7e0ef92ca49e824f697b8140a5
happycodez <happybrown2k@gmail.com>
2024-08-26 07:21:38 +0200

    showName

--- components/shared/sidebar.tsx
+++ components/shared/sidebar.tsx
@@ -60,8 +60,8 @@ const Sidebar = () => {
                                 )
                             })}
 
-                            <li className='flex-center cursor-pointer gap-2 p-4'>
-                                <UserButton afterSignOutUrl='/' />
+                            <li className='flex-center cursor-pointer gap-2 p-4 capitalize'>
+                                <UserButton afterSignOutUrl='/' showName />
                             </li>
                         </ul>
                     </SignedIn>
